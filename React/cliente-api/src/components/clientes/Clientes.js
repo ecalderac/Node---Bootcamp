bb3c6b4daff27bcf3be3d50a1e43b8efb5396bc2
ecalderac <e.calderac@hotmail.com>
2021-03-05 12:08:22 -0300

    commit 3ero react

--- React/cliente-api/src/components/clientes/Clientes.js
+++ React/cliente-api/src/components/clientes/Clientes.js
@@ -1,6 +1,44 @@
-import React from "react";
+import React, { useEffect, useState, Fragment } from "react";
+
+//importar componentes
+import Cliente from "./Cliente";
+
+//importar cliente axios
+import clienteAxios from "../../config/axios";
+
+import { Link } from "react-router-dom";
 
 function Clientes() {
-  return <h2>Clientes</h2>;
+  //Trabajar con el useState
+  //clientes = state, guardarClientes = funcion para guardar el useState
+  const [clientes, guardarClientes] = useState([]);
+
+  //Query a la API
+  const consultarAPI = async () => {
+    const clientesConsulta = await clienteAxios.get("/clientes");
+    //console.log(clientesConsulta.data);
+    guardarClientes(clientesConsulta.data);
+  };
+
+  //use effect es similar a componentdidmount y willmount
+  useEffect(() => {
+    consultarAPI();
+  }, []);
+
+  return (
+    <Fragment>
+      <h2>Clientes</h2>
+      <Link to={"/clientes/nuevo"} class="btn btn-verde nvo-cliente">
+        {" "}
+        <i class="fas fa-plus-circle"></i>
+        Nuevo Cliente
+      </Link>
+      <ul className="listado-clientes">
+        {clientes.map((cliente) => (
+          <Cliente key={cliente._id} cliente={cliente} />
+        ))}
+      </ul>
+    </Fragment>
+  );
 }
 export default Clientes;
